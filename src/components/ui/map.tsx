bc3f0d545b90e4203a96e6e9928b0d2d268868f5
Jahid Hossen <jahid.hossen.me@gmail.com>
2026-02-26 17:01:37 +0600

    battles map view solved

---
+++ src/components/ui/map.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+export {
+  Map,
+  MapTileLayer,
+  MapMarker,
+  MapPopup,
+  MapZoomControl,
+  MapLayerGroup,
+  MapLayers,
+  MapLayersControl,
+  MapFeatureGroup,
+  MapMarkerClusterGroup,
+  MapCircle,
+  MapCircleMarker,
+  MapPolyline,
+  MapPolygon,
+  MapRectangle,
+  MapFullscreenControl,
+  MapLocateControl,
+  MapSearchControl,
+  MapControlContainer,
+  MapDrawControl,
+  MapDrawPolyline,
+  MapDrawCircle,
+  MapDrawRectangle,
+  MapDrawPolygon,
+} from '@shadcn-map/map';
+
